515347885c001bdf05717f25a51d5e19288e8957
Albert Cedeno <albertcdn@gmail.com>
2020-08-02 10:50:31 -0400

    fixed

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-// var http = require('http');
-// var fs = require('fs');
+var http = require('http');
+var fs = require('fs');
 
-// const PORT=8080; 
+const PORT=8080; 
 
-// fs.readFile('./index.html', function (err, html) {
+fs.readFile('./index.html', function (err, html) {
 
-//     if (err) throw err;    
+    if (err) throw err;    
 
-//     http.createServer(function(request, response) {  
-//         response.writeHeader(200, {"Content-Type": "text/html"});  
-//         response.write(html);  
-//         response.end();  
-//     }).listen(PORT);
-// });
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(PORT);
+});
 
 
-// var resemble = require('resemblejs');
+var resemble = require('resemblejs');
 
 // var diff = resemble("https://i.imgur.com/WlGDDvw.png")
     // .compareTo(dataURL)
