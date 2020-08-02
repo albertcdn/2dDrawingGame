13cf6bc894c2f9d2807b47e6bb7afb9bf065615a
Albert Cedeno <albertcdn@gmail.com>
2020-08-02 10:49:56 -0400

    server starts, data saved to variable

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-var http = require('http');
-var fs = require('fs');
+// var http = require('http');
+// var fs = require('fs');
 
-const PORT=8080; 
+// const PORT=8080; 
 
-fs.readFile('./index.html', function (err, html) {
+// fs.readFile('./index.html', function (err, html) {
 
-    if (err) throw err;    
+//     if (err) throw err;    
 
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT);
-});
+//     http.createServer(function(request, response) {  
+//         response.writeHeader(200, {"Content-Type": "text/html"});  
+//         response.write(html);  
+//         response.end();  
+//     }).listen(PORT);
+// });
 
 
-var resemble = require('resemblejs');
+// var resemble = require('resemblejs');
 
-var diff = resemble("https://i.imgur.com/WlGDDvw.png")
-    .compareTo(dataURL)
-    .ignoreColors()
-    .onComplete(function(data) {
-        console.log(data);
-        /*
-	{
-	  misMatchPercentage : 100, // %
-	  isSameDimensions: true, // or false
-	  dimensionDifference: { width: 0, height: -1 }, // defined if dimensions are not the same
-	  getImageDataUrl: function(){}
-	}
-	*/
-    });
+// var diff = resemble("https://i.imgur.com/WlGDDvw.png")
+    // .compareTo(dataURL)
+    // .ignoreColors()
+    // .onComplete(function(data) {
+    //     console.log(data);
+    //     /*
+	// {
+	//   misMatchPercentage : 100, // %
+	//   isSameDimensions: true, // or false
+	//   dimensionDifference: { width: 0, height: -1 }, // defined if dimensions are not the same
+	//   getImageDataUrl: function(){}
+	// }
+	// */
+    // });
